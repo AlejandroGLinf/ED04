899c7e3161ec0bfba973300176039aa4567039dc
Development <Development@10.0.2.15>
2022-02-14 19:38:53 +0100

    Hemos introducido comentarios JavaDoc a la clase CCuenta.java

--- src/main/java/Cuentas/CCuenta.java
+++ src/main/java/Cuentas/CCuenta.java
@@ -1,78 +1,109 @@
 package Cuentas;
 
 /**
- *
+ * Esta clase contiene los métodos necesarios para trabajar con el objeto CCuenta
  * @author Alejandro García López
+ * @version Añadimos version con comentarios a github
  */
 
 public class CCuenta {
 
     /**
-     * @return the nombre
+     * Nombre del titular de la cuenta
+     */
+    private String nombre;
+    /**
+     * Identificador de la cuenta
+     */
+    private String cuenta;
+    /**
+     * Registro del salto de cuenta
+     */
+    private double saldo;
+    /**
+     * Interés aplicado a la cuenta
+     */
+    private double tipoInterés;
+
+    /**
+     * Devuelve el atributo nombre
+     * @return nombre del titular
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Establece el nombre del titular de la cuenta
+     * @param nombre de la cuenta
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * Devuelve el identificador de la cuenta
+     * @return cuenta del titular
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Actualiza el número de cuenta
+     * @param cuenta número de cuenta 
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Devuelve el saldo actual de la cuenta
+     * @return saldo de la cuenta
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Actualiza el saldo de la cuenta
+     * @param saldo edita el saldo de la cuenta
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInterés
+     * Consulta la cantidad de interés
+     * @return devuelve la cantidad de interés
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
     /**
-     * @param tipoInterés the tipoInterés to set
+     * Establece el interés de la cuenta
+     * @param tipoInterés nueva cantidad interés
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
-
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInterés;
-
+    
+    /**
+     * Constructor sin parámetros
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Constructor con todos los parámetros     * 
+     * @param nom Nombre del titular de la cuenta
+     * @param cue Identificador de la cuenta
+     * @param sal Saldo inicial
+     * @param tipo Tipo de interés que tendrá la cuenta
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -80,11 +111,20 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Consulta el saldo actual de la cuenta
+     * @return getSaldo Devuelve el saldo actual de la cuenta
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Realiza un ingreso en la cuenta, para sumar la cantidad a la actual
+     * @param cantidad que se sumará a la cuenta
+     * @throws java.lang.Exception lanza excepción si el número es negativo
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -92,6 +132,11 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Realiza una retirada de saldo en la cuenta, resta la cantidad al saldo actual
+     * @param cantidad que se retirará de la cuenta
+     * @throws java.lang.Exception lanza excepción si el número es negativo y si la cantidad es menor que el saldo actual
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
